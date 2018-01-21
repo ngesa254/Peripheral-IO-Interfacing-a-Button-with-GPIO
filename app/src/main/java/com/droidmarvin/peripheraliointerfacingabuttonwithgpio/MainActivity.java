f58bc703a488b10065a84301cd652780deb3db10
ngesa marvin <marvinngesa@gmail.com>
2018-01-21 06:24:21 +0300

    error correction

--- app/src/main/java/com/droidmarvin/peripheraliointerfacingabuttonwithgpio/MainActivity.java
+++ app/src/main/java/com/droidmarvin/peripheraliointerfacingabuttonwithgpio/MainActivity.java
@@ -2,10 +2,9 @@ package com.droidmarvin.peripheraliointerfacingabuttonwithgpio;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.things.pio.PeripheralManagerService;
-
-import sun.rmi.runtime.Log;
 
 public class MainActivity extends Activity {
 
@@ -13,7 +12,6 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         PeripheralManagerService service = new PeripheralManagerService();
         Log.d (TAG, "Available GPIO: " + service.getGpioList());
 
