ae389cb08734bb2f1e24a1e9c8af1be67763b2e2
ngesa marvin <marvinngesa@gmail.com>
2018-01-21 06:31:52 +0300

    Create Button GPIO connection

--- app/src/main/java/com/droidmarvin/peripheraliointerfacingabuttonwithgpio/MainActivity.java
+++ app/src/main/java/com/droidmarvin/peripheraliointerfacingabuttonwithgpio/MainActivity.java
@@ -4,16 +4,32 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.android.things.pio.Gpio;
 import com.google.android.things.pio.PeripheralManagerService;
+
+import java.io.IOException;
 
 public class MainActivity extends Activity {
 
     private static final String TAG = "MainActivity";
+
+    private static final String GREEN_LED_PIN = "BCM19";
+
+    private Gpio mButtonGio;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         PeripheralManagerService service = new PeripheralManagerService();
-        Log.d (TAG, "Available GPIO: " + service.getGpioList());
+        //Log.d (TAG, "Available GPIO: " + service.getGpioList());
+
+        //Create Button GPIO connection.
+        try {
+            mButtonGio = service.openGpio(GREEN_LED_PIN);
+        }catch (IOException e){
+            throw new IllegalStateException(mButtonGio +"error opening the button",e);
+        }
 
     }
 }
